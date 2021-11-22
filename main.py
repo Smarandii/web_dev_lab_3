c57faac0276a9cc4b4055772b821233f3137bab3
Oleg Smarandi <olegsmarandi@gmail.com>
2021-11-22 12:21:51 +0300

    new pages added: user page publication page

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from tmp import publications, users
 app = Flask(__name__)
 
 
@@ -6,36 +7,52 @@ app = Flask(__name__)
 @app.route('/home')
 def home_page():
     """
-    Функция, которая рендерит страницу http://localhost:5000/ или http://localhost:5000/home
+        Функция, которая рендерит страницу http://localhost:5000/ или http://localhost:5000/home
     """
     return render_template("home.html")
 
 
-@app.route('/publications')
+@app.route('/<username>')
+@app.route('/home/<username>')
+def username_page(username):
+    return render_template("user.html", username=username, users=users, publications=publications)
+
+
+@app.route('/publications/')
 def publications_page():
     """
-    Функция, которая рендерит страницу http://localhost:5000/publications
+        Функция, которая рендерит страницу http://localhost:5000/publications
     """
-    publications = [
-        {"id": 1, "author": "Имя Автора", "post_title": "Заголовок поста", "post_txt": "Текст поста", "publication_date": "Дата публикации", "tags": ["кяввм"]},
-        {"id": 2, "author": "Имя Автора", "post_title": "Заголовок поста", "post_txt": "Текст поста", "publication_date": "Дата публикации", "tags": ["кяввм"]},
-        {"id": 3, "author": "Имя Автора", "post_title": "Заголовок поста", "post_txt": "Текст поста", "publication_date": "Дата публикации", "tags": ["кяввм"]}
-    ]
     return render_template('publications.html', publications=publications)
+
+
+@app.route('/publications/<int:post_id>')
+def publication_page(post_id):
+
+    return render_template('publication.html', post_id=post_id, publications=publications)
 
 
 @app.route('/tags')
 def tags_page():
+    """
+        Функция, которая рендерит страницу http://localhost:5000/tags
+    """
     return render_template('tags.html')
 
 
 @app.route('/login')
 def login_page():
+    """
+        Функция, которая рендерит страницу http://localhost:5000/login
+    """
     return render_template('login.html')
 
 
 @app.route('/register')
 def register_page():
+    """
+        Функция, которая рендерит страницу http://localhost:5000/register
+    """
     return render_template('register.html')
 
 
